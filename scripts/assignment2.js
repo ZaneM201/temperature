e3e64a711ee9e18c9dd04e3de31c71284f8edf8f
Zane Moon <zanem201@gmail.com>
2025-07-23 15:05:27 -0700

    Needs picture in header

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -1,4 +1,4 @@
 function convertToFahrenheit(){
-    let celsius = prompt("Enter Celsius");
-    document.getElementById("temp").innerHTML += `<li class ="item"> Fahrenheit: ${(celsius*9/5 )+32}</li>`; 
+    let celsius = prompt("Enter Celsius:");
+    document.getElementById("temp").innerHTML += `<li class ="item"> The conversion of Celsius ${celsius}" to Fahrenhiet is ${(celsius*9/5 )+32}"</li>`; 
 }
