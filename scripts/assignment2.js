de628a8a0aa8192efb87b5fd92a1f0827a25fec6
Zane Moon <zanem201@gmail.com>
2025-07-23 01:37:34 -0700

    addignment half way?

---
+++ scripts/assignment2.js
@@ -0,0 +1,4 @@
+function convertToFahrenheit(){
+    let fahrenhiet = prompt("Enter Celsius");
+    document.getElementById(result).innerHTML `<li class ="temp"> Fahrenheit: ${Celcius*9/5) + 32}</li>` 
+}
