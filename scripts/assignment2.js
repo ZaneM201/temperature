d622ba971c4480ad445e50ea64d17d07462f2828
Zane Moon <zanem201@gmail.com>
2025-07-24 17:13:46 -0700

    temp assignment done.

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -1,4 +1,4 @@
 function convertToFahrenheit(){
     let celsius = prompt("Enter Celsius:");
-    document.getElementById("temp").innerHTML += `<li class ="item"> The conversion of Celsius ${celsius}" to Fahrenhiet is ${(celsius*9/5 )+32}"</li>`; 
+    document.getElementById("temp").innerHTML += `<li class ="item"> The conversion of Celsius ${celsius}" to Fahrenhiet is ${(celsius*9/5)+32}"</li>`; 
 }
