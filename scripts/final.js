60a60e3d1769b8ce01003e588c038bd8972ed120
Zane Moon <zanem201@gmail.com>
2025-07-27 16:47:09 -0700

    Table needs CSS and thermometer needs to work.

--- scripts/final.js
+++ scripts/final.js
@@ -8,3 +8,16 @@ function fahrenheitToCelsius(){
     let f = prompt("Enter Fahrenheit:");
     p.innerHTML += `${f} °F = ${(f-32)*5/9} °C`;
 }
+
+
+
+function generateTable(){
+    let tbody = document.getElementById("tempTable");
+    for(let c=0; c<=100; c+=10){
+        let f = (c*9/5)+32
+        tbody.innerHTML += `<tr>
+            <td> ${c} </td>
+            <td> ${f} </td>
+        </tr>`
+    }
+}
