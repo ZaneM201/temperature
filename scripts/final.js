5b32a64d8cfc62444bd1e7a80911439eca4a1905
Zane Moon <zanem201@gmail.com>
2025-07-28 16:07:12 -0700

    103 Final is done!!

--- scripts/final.js
+++ scripts/final.js
@@ -7,32 +7,43 @@ function celsiusToFahrenheit(){
     let c = prompt("Enter Celsius:");
     p.innerHTML += `${c} °C = ${(c*9/5)+32} °F`;
     let thermometer = document.getElementById("thermo");
+    let fillLevel=Math.round((c/100)*10)
     for(let i=0; i<10; i++){
         let level = document.createElement("div");
         level.classList.add("level");
-        if(i<3, c<11){
-            level.style.backgroundColor = "blue";
-        }else if(i<7, c<26){
-            level.style.backgroundColor = "orange";
+        if (i<fillLevel){
+            if (c<11){
+                level.style.backgroundColor = "blue";
+            }else if(c<26){
+                level.style.backgroundColor = "orange";
+            }else{
+                level.style.backgroundColor = "red";
+            }
         }else{
-            level.style.backgroundColor = "red";
+            level.style.backgroundColor = "#dedede"; 
         }
         thermometer.appendChild(level);
     }
 }
 function fahrenheitToCelsius(){
     let f = prompt("Enter Fahrenheit:");
-    p.innerHTML += `${f} °F = ${(f-32)*5/9} °C`;
+    let c = (f-32)*5/9;
+    p.innerHTML += `${f} °F = ${c.toFixed(1)} °C`;
     let thermometer = document.getElementById("thermo");
+    let fillLevel=Math.round((c/100)*10)
     for(let i=0; i<10; i++){
         let level = document.createElement("div");
         level.classList.add("level");
-        if(i<3, (f-32)*5/9<11){
-            level.style.backgroundColor = "blue";
-        }else if(i<7, (f-32)*5/9<26){
-            level.style.backgroundColor = "orange";
+        if (i<fillLevel){
+            if(c<11){
+                level.style.backgroundColor = "blue";
+            }else if(c<26){
+                level.style.backgroundColor = "orange";
+            }else{
+                level.style.backgroundColor = "red";
+            }
         }else{
-            level.style.backgroundColor = "red";
+            level.style.backgroundColor = "#dedede"; 
         }
         thermometer.appendChild(level);
     }
