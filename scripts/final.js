b0bfefeee3e43a83417817ff8746ccd7b3de35fe
Zane Moon <zanem201@gmail.com>
2025-07-28 14:56:25 -0700

    thermometer changes color for C

--- scripts/final.js
+++ scripts/final.js
@@ -6,33 +6,45 @@ function clearResults(){
 function celsiusToFahrenheit(){
     let c = prompt("Enter Celsius:");
     p.innerHTML += `${c} °C = ${(c*9/5)+32} °F`;
+    let thermometer = document.getElementById("thermo");
+    for(let i=0; i<10; i++){
+        let level = document.createElement("div");
+        level.classList.add("level");
+        if(i<3, c<11){
+            level.style.backgroundColor = "blue";
+        }else if(i<7, c<26){
+            level.style.backgroundColor = "orange";
+        }else{
+            level.style.backgroundColor = "red";
+        }
+        thermometer.appendChild(level);
+    }
 }
 function fahrenheitToCelsius(){
     let f = prompt("Enter Fahrenheit:");
     p.innerHTML += `${f} °F = ${(f-32)*5/9} °C`;
     
 }
-function fillThermo(){
+/* Function fillThermo(c){
     let thermo = document.getElementById("thermo");
-    for(let i=0; i<10; i++){
+    for(let c=0; c<10; c++){
         let level = document.createElement("div");
         level.classList.add("level");
-        if(i<=10){
+        if(c<=3){
            level.style.backgroundColor = "blue";
-        }else if(i<=25){
+        }else if(c<=7){
             level.style.backgroundColor = "orange";
         }else{
             level.style.backgroundColor = "red";
         }
         thermo.appendChild(level);
     }
-}
+} */
+let tbody = document.getElementById("tempTable");
 function clearTable(){
-    let tbody = document.getElementById("tempTable");
     tbody.innerHTML="";
 }
 function generateTable(){
-    let tbody = document.getElementById("tempTable");
     for(let c=0; c<=100; c+=10){
         let f = (c*9/5)+32
         tbody.innerHTML += `<tr>
