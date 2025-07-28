6266b145ec52be74b5755a634a27884b5bd76396
Zane Moon <zanem201@gmail.com>
2025-07-28 15:14:41 -0700

    Thermometer kinda works

--- scripts/final.js
+++ scripts/final.js
@@ -23,23 +23,20 @@ function celsiusToFahrenheit(){
 function fahrenheitToCelsius(){
     let f = prompt("Enter Fahrenheit:");
     p.innerHTML += `${f} °F = ${(f-32)*5/9} °C`;
-    
-}
-/* Function fillThermo(c){
-    let thermo = document.getElementById("thermo");
-    for(let c=0; c<10; c++){
+    let thermometer = document.getElementById("thermo");
+    for(let i=0; i<10; i++){
         let level = document.createElement("div");
         level.classList.add("level");
-        if(c<=3){
-           level.style.backgroundColor = "blue";
-        }else if(c<=7){
+        if(i<3, (f-32)*5/9<11){
+            level.style.backgroundColor = "blue";
+        }else if(i<7, (f-32)*5/9<26){
             level.style.backgroundColor = "orange";
         }else{
             level.style.backgroundColor = "red";
         }
-        thermo.appendChild(level);
+        thermometer.appendChild(level);
     }
-} */
+}
 let tbody = document.getElementById("tempTable");
 function clearTable(){
     tbody.innerHTML="";
